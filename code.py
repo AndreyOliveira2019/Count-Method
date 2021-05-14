188f724e9e35f83a10c54d4dcaa2c350b62c9a92
AndreyOliveira2019 <55673289+AndreyOliveira2019@users.noreply.github.com>
2021-05-13 23:33:09 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+#contador de caracteres na lista.
+
+teste = [1,2,3,4,5,6]
+count = 0
+for c in teste:
+    count = count + 1
+print(count) 
+
+#também serve com strings
+
+teste = 'teste'
+count = 0
+for c in teste:
+    count = count + 1
+print(count) 
